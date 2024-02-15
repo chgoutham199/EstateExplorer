3a039029cd8d9f6b5cbca02b6b8f4d582f8ad5e3
chgoutham199 <chgoutham199@gmail.com>
2024-02-15 23:24:05 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>
+{
+    console.log("connected to database");
+}).catch((err)=>
+{
+    console.log(err);
+});
 
 const app=express();
 app.listen(3000,(req,res)=>
@@ -6,3 +18,4 @@ app.listen(3000,(req,res)=>
     console.log("server running on port 3000 !");
 }
 );
+
