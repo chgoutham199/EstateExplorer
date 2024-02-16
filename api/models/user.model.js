9039a4c42ff5f2a2ccaf79254f1d081389f01fbc
chgoutham199 <chgoutham199@gmail.com>
2024-02-16 14:16:11 +0530

    create user schema

---
+++ api/models/user.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { string } from "postcss-selector-parser";
+
+const userSchema= new mongoose.Schema({
+    username:{
+        type:String,
+        required:[true,"Please provide a Username"],
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+       
+    },
+},{timestamps:true});
+
+const User = mongoose.model('User',userSchema);
+export default user;
