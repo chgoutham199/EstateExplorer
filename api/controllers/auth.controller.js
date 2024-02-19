2b639058b5314a9f5438016f65599831df78477a
chgoutham199 <chgoutham199@gmail.com>
2024-02-19 23:26:43 +0530

    1 day off

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -32,8 +32,7 @@ export const signin=async (req,res,next)=>
          const {password:pass, ...rest}=validUser._doc;
          res.cookie('access_token',token,{httpOnly:true})
          .status(200).json(rest);
-
-        } catch (error) {
+  } catch (error) {
         next(error);
      }
 
