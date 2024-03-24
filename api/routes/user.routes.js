1eb9fdd0c09d5c39b9dd74c23e92168c09c625b6
Alayna Shaheen <alaynashaheen2@gmail.com>
2024-03-24 19:36:23 +0530

    created api route for get user listings

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { test,  updateUser,deleteUser } from '../controllers/user.controller.js';
+import { test,  updateUser,deleteUser,getUserListings } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const userRouter=express.Router();
 userRouter.get("/test",test);
 userRouter.post("/update/:id",verifyToken,updateUser);
 userRouter.delete("/delete/:id",verifyToken,deleteUser);
+userRouter.get('/listing/:id',verifyToken,getUserListings);
 export default userRouter; 
