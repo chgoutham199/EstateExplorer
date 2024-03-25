9cb47d233ea6f3a14c6ec19eae015bb6867560a2
Alayna Shaheen <alaynashaheen2@gmail.com>
2024-03-25 22:17:58 +0530

    complete listings age

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test,  updateUser,deleteUser,getUserListings } from '../controllers/user.controller.js';
+import { test,  updateUser,deleteUser,getUserListings, getUser} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const userRouter=express.Router();
@@ -7,4 +7,5 @@ userRouter.get("/test",test);
 userRouter.post("/update/:id",verifyToken,updateUser);
 userRouter.delete("/delete/:id",verifyToken,deleteUser);
 userRouter.get('/listings/:id',verifyToken,getUserListings);
+userRouter.get('/:id', verifyToken, getUser);
 export default userRouter; 
